bfe75a92b926528d6ad52449e81f922829d4927b
Kevin Chiang <kevinwchiang@gmail.com>
2017-10-11 02:18:45 -0700

    cleanup, linting

--- src/client/Player.js
+++ src/client/Player.js
@@ -1,30 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Player extends Component {
-  render() {
-    const { currentStation, selectTag } = this.props;
-    if (currentStation) {
-      return (
-        <div className="player-container">
-          <h2>{currentStation.name}</h2>
-          <audio
-            controls
-            src={currentStation.streamUrl}
-            autoPlay
-          >
-            Your browser does not support the <code>audio</code> element.
-          </audio>
-          <h4>Tags</h4>
-          {currentStation.tags.map(tag => (
-            <div key={tag}>
-              <span className="tag" onClick={() => selectTag(tag)}>{tag}</span>
-            </div>
-            ))}
-        </div>
-      );
-    }
-    return null;
+const Player = (props) => {
+  const { currentStation, selectTag } = props;
+  if (currentStation) {
+    return (
+      <div className="player-container">
+        <h2>{currentStation.name}</h2>
+        <audio
+          controls
+          src={currentStation.streamUrl}
+          autoPlay
+        >
+          Your browser does not support the <code>audio</code> element.
+        </audio>
+        <h4>Tags</h4>
+        {currentStation.tags.map(tag => (
+          <div key={tag}>
+            <button className="tag" onClick={() => selectTag(tag)}>{tag}</button>
+          </div>
+          ))}
+      </div>
+    );
   }
-}
+  return null;
+};
+
+Player.defaultProps = {
+  currentStation: {},
+  selectTag: null,
+};
+
+Player.propTypes = {
+  currentStation: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    popularity: PropTypes.number.isRequired,
+    reliability: PropTypes.number.isRequired,
+    streamUrl: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string),
+  }),
+  selectTag: PropTypes.func,
+};
 
 export default Player;
--- src/client/Stations.js
+++ src/client/Stations.js
@@ -1,31 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Stations extends Component {
-  render() {
-    const {
-      data, handleClick, filter, resetFilter,
-    } = this.props;
-    let stations = data;
-    if (filter) {
-      stations = stations.filter(station => station.tags.indexOf(filter) !== -1);
-    }
-    return (
-      <div className="stations-container">
-        <div className="stations-header">
-          <h2 style={{ display: 'inline-block' }}>
-            Stations
-            {filter && <span>({filter})</span>}
-          </h2>
-          <button className="reset-button" onClick={resetFilter}>reset filter</button>
-        </div>
-        {stations.map(station => (
-          <div key={station.id} className="station-button" onClick={() => handleClick(station)}>
-            {station.name}
-          </div>
-          ))}
-      </div>
-    );
+const Stations = (props) => {
+  const {
+    data, handleClick, filter, resetFilter,
+  } = props;
+  let stations = data;
+  if (filter) {
+    stations = stations.filter(station => station.tags.indexOf(filter) !== -1);
   }
-}
+  return (
+    <div className="stations-container">
+      <div className="stations-header">
+        <h2 style={{ display: 'inline-block' }}>
+          Stations
+          {filter && <span>({filter})</span>}
+        </h2>
+        <button className="reset-button" onClick={resetFilter}>reset filter</button>
+      </div>
+      {stations.map(station => (
+        <div
+          key={station.id}
+          className="station-button"
+          onClick={() => handleClick(station)}
+        >
+          {station.name}
+        </div>
+        ))}
+    </div>
+  );
+};
+
+Stations.defaultProps = {
+  data: [],
+  handleClick: null,
+  resetFilter: null,
+  filter: null,
+};
+
+Stations.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  handleClick: PropTypes.func,
+  resetFilter: PropTypes.func,
+  filter: PropTypes.string,
+};
 
 export default Stations;
